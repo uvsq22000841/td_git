03c5f3427303e183df6a9ee4238774679384ad4f
Harnoufi Salah Eddine <salah-eddine.harnoufi@ens.uvsq.fr>
2020-12-17 15:13:55 +0000

    version a prendre

--- calculatrice.py
+++ calculatrice.py
@@ -10,7 +10,7 @@ canvas1 = tk.Canvas(racine, width= 500, height=500, bg="black")
 canvas1.grid()
 
 label1= tk.Label(racine, text="Bienvenue sur la calculatrice")
-Label1.grid()
+label1.grid()
 
 
 carac = ""
@@ -23,86 +23,114 @@ def afficher(c):
 def effacer():
     global carac
     if len(carac) != 0:
-        carac.pop(-1)
+        carac = carac[0:-2]
+
+    afficher(carac)
 
 def caractere(c):
-    global carac, a
+    global carac, a, reponse, resultat
 
     if a == 0:
         a = 1
         if c not in ['+','-','*','/']:
             carac = carac + c
+            afficher(carac)
+        else:
+            carac = str(resultat) + c
+            afficher('rep' + c)
     else:
         carac = carac + c
         afficher(carac)
 
+def reset():
+    global carac, a
+    carac= ''
+    a= 0
+    reponse = 0
 
 def calcule():
-    global carac, a, resultat
-    a= carac.count('+') + carac.count('-') + carac.count('*') + carac.count('/') 
+    global carac, a, resultat, reponse
 
 
+    j= carac.count('+') + carac.count('-') + carac.count('*') + carac.count('/') 
 
-    if carac.count>1 or carac.count == 0 or carac[0] in ['+', '-', '*', '+'] or carac[-1] in ['+', '-', '*', '+']:
+
+    if j > 1 or j == 0 or carac[0] in ['+', '-', '*', '+'] or carac[-1] in ['+', '-', '*', '+']:
         afficher('error')
+        reset()
 
     else:
+
+
+
+
         for i in range(len(carac)):
             if carac[i] in ['+', '-', '*', '/']:
                 A = carac[0:i]
-                B = carac[i+1: -1]
+                B = carac[i+1: -1] + carac[-1]
                 Z= carac[i]
                 break
 
+        
+        O= list(A)
+        V= list(B)
+
+        for i in range(len(O)):
+            if '_' == O[i]:
+                O[i] = '-'
+
+
+
+        for i in range(len(V)):
+            if '_' in V:
+                V[i] = '-'
+        
+
+        
+        print(A)
+        print(B)
+                
+
+                
+
+
+
         try:
+
+
             A= float(A)
-            B= float(A)
+            B= float(B)
+
             if(Z == '+'):
                 resultat = A + B
                 afficher(resultat)
+                reset()
+                reponse = 1
             elif(Z == '-'):
                 resultat = A - B
                 afficher(resultat)
+                reset()
+                reponse = 1
+
             elif(Z == '*'):
                 resultat = A * B
                 afficher(resultat)
+                reset()
+                reponse = 1
             else:
                 if B == 0:
                     afficher('error')
+                    reset()
                 else:
                     resultat = A / B
                     afficher(resultat)
+                    reset()
+                    reponse = 1
 
 
         except:
             afficher('error')
-
-
-
-          
-            
-
-
-        a cette premiere occurence placer, la partie gauche dans partieGaucge
-        et la partie droite dans partieDroit
-
-        si la partie gauche n'est pas un int ou float ou si la partie droit n'est pas une int ou float
-            afficher(error)
-            a=1
-        sinon
-            si la premier occurence trouvée a été un fois, moins ou plus:
-                enregistrer= A + B
-            sinon 
-                enregistrer= A* B
-            sinon enregistrer = A-B
-            sinon
-                si B =0
-                    afficher(error infini)
-                    a=1
-                sinon enregistrer= A/B
-        
-
-
+            reset()
 
 
 
@@ -127,12 +155,14 @@ button6.grid()
 button7= tk.Button(canvas1, text="7", command= lambda:caractere("7"))
 button7= tk.Button(canvas1, text="7", command= lambda:caractere("7"))
 
-button8= tk.Button(canvas1, text="8", command= lambda:caractere(8))
+button8= tk.Button(canvas1, text="8", command= lambda:caractere("8"))
 button8.grid()
 
 button9= tk.Button(canvas1, text="9", command= lambda:caractere("9"))
 button9.grid()
 
+button0= tk.Button(canvas1, text="0", command= lambda:caractere("0"))
+button0.grid()
 
 buttonVirgule = tk.Button(canvas1, text=".", command= lambda:caractere("."))
 buttonVirgule.grid()
@@ -143,6 +173,9 @@ buttonplus.grid()
 buttonmoins = tk.Button(canvas1, text="-", command= lambda: caractere("-"))
 buttonmoins.grid()
 
+buttondepmoins = tk.Button(canvas1, text="_", command= lambda: caractere("_"))
+buttondepmoins.grid()
+
 buttonfois = tk.Button(canvas1, text="*", command= lambda:caractere("*"))
 buttonfois.grid()
 
@@ -152,8 +185,8 @@ buttondiv.grid()
 labeltext= tk.Label(canvas1, text="bienvenue sur la calculatrice")
 labeltext.grid()
 
-calcule = tk.Button(canvas1, text="=", command= lambda:calcule())
-calcule.grid()
+buttoncalcule = tk.Button(canvas1, text="=", command= lambda:calcule())
+buttoncalcule.grid()
 
 
 less = tk.Button(canvas1, text="effacer", command= lambda:effacer())
